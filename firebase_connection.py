a55501942cf6cb6e10cd7dd8bb19102bb598ee3d
Miller-Vidal <211102@ids.upchiapas.edu.mx>
2023-07-23 05:09:56 -0600

    prueba 5 creo

--- firebase_connection.py
+++ firebase_connection.py
@@ -4,9 +4,14 @@ from firebase_admin import credentials, firestore, initialize_app
 
 # Ruta al archivo de configuración de Firebase descargado
 cred = credentials.Certificate("imagensCout.json")
-# Inicializar la aplicación de Firebase con el nombre del bucket
+
+# Especifica el ID de tu proyecto de Firebase
+project_id = "back-flutter-a83ed"  # Reemplaza "your-project-id" con el ID de tu proyecto
+
+# Inicializar la aplicación de Firebase con el nombre del bucket y el proyecto
 initialize_app(cred, options={
-    'storageBucket': 'back-flutter-a83ed.appspot.com'
+    'storageBucket': 'back-flutter-a83ed.appspot.com',
+    'projectId': project_id
 })
 
 # Obtén una referencia a la colección en Firestore
