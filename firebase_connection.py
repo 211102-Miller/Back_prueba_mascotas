84e42c9caf6d4d7b1fc5d0fb915c466ff6b1c130
Miller-Vidal <211102@ids.upchiapas.edu.mx>
2023-07-23 22:29:41 -0600

    esperemos que  bien

--- firebase_connection.py
+++ firebase_connection.py
@@ -1,16 +1,20 @@
 # firebase_connection.py
 import firebase_admin
 from firebase_admin import credentials, firestore, initialize_app
+from dotenv import dotenv_values
+
+# Cargar las variables de entorno desde el archivo .env
+env_variables = dotenv_values('venv/.env_todo')
 
 # Ruta al archivo de configuración de Firebase descargado
-cred = credentials.Certificate("imagensCout.json")
+cred = credentials.Certificate(env_variables.get('JSON_FIREBASE'))
 
 # Especifica el ID de tu proyecto de Firebase
 project_id = "back-flutter-a83ed"  # Reemplaza "your-project-id" con el ID de tu proyecto
 
 # Inicializar la aplicación de Firebase con el nombre del bucket y el proyecto
 initialize_app(cred, options={
-    'storageBucket': 'back-flutter-a83ed.appspot.com',
+    'storageBucket': env_variables.get('BUCKET'),
     'projectId': project_id
 })
 
